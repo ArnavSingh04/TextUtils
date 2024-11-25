72bba3cef1877cc7cc71eb3beb1969ab23857c7d
arnavsingh04 <arnavsingh.sethi@gmail.com>
2024-11-25 23:06:51 +1100

    how modules work in js, and using some autofill extension

---
+++ my-app/module1.mjs
@@ -0,0 +1,8 @@
+let a = 'Arnav'
+let b = 'Virat'
+let c = 'Rohit'
+
+export default a;
+
+export {b};
+export {c};
---
+++ my-app/module2.mjs
@@ -0,0 +1,5 @@
+import djz, {b,c} from './module1.mjs'
+
+console.log(djz);
+console.log(b);
+console.log(c);
